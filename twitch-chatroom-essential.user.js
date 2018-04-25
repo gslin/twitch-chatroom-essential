dcf03ef7f50c120dafcf1ae45c7a4679394f7144
Gea-Suan Lin <gslin@gslin.org>
2018-04-26 00:40:50 +0800

    Bugfix for channel switching.

--- twitch-chatroom-essential.user.js
+++ twitch-chatroom-essential.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Chatroom Essential
 // @namespace    https://wiki.gslin.org/wiki/TwitchChatroomEssential
-// @version      0.2018.0426
+// @version      0.2018.0426.1
 // @description  Show users with badge only.
 // @author       Gea-Suan Lin <darkkiller@gmail.com>
 // @match        https://www.twitch.tv/*
@@ -20,7 +20,6 @@
     sheet.innerHTML = '#toggle_essential {padding-left:0.5em;width: 100%;}\n#toggle_essential input {vertical-align: middle;}';
     document.getElementsByTagName('head')[0].appendChild(sheet);
 
-    let opt_done = false;
     let opt = document.createElement('div');
     opt.setAttribute('id', 'toggle_essential');
     opt.innerHTML = '<input id="toggle_essential_checkbox" type="checkbox"> <label for="toggle_essential_checkbox">Show essential messages only</label>';
@@ -35,10 +34,9 @@
     let ctx = document.getElementById('root');
 
     let ob = new window.MutationObserver(function(events){
-        if (!opt_done) {
+        if (0 === jQuery('#toggle_essential').length) {
             let el = jQuery('div.chat-input.tw-pd-b-2.tw-pd-x-2');
             if (el.length) {
-                opt_done = true;
                 el.append(opt);
             }
         }
